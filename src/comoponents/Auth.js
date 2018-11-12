ee462cab58003697cc9ed782d7ddf911f8672d2b
SeongkyuLee <seongkyu0617@gmail.com>
2018-11-12 16:10:03 +0900

    feat: add Auth related component

--- src/comoponents/Auth.js
+++ src/comoponents/Auth.js
@@ -6,15 +6,39 @@ class Auth extends Component {
     componentDidMount() {
         this.props.fetchUser()
     }
+    renderContent() {
+        const { auth } = this.props
+        
+        switch(auth) {
+            case null:
+                return 'Still Deciding'
+            case false:
+                return <a href='/auth/google'>Login With Google</a>
+            default:
+                return (
+                    <div>
+                        <span>Hello {auth.googleId} </span>
+                        <a href='/api/logout'>Logout</a>
+                    </div>
+                )
+        }
+    }
     render() {
         return (
             <div>
-                <a href='/auth/google'>
-                    Login with google
-                </a>
+                {this.renderContent()}
+
             </div>
         )        
     }
 }
 
-export default connect(null, actions)(Auth)
+function mapStateToProps(state) {
+    const { auth } = state
+    
+    return {
+        auth
+    }
+}
+
+export default connect(mapStateToProps, actions)(Auth)
